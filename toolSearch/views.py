11d18ebcf130922485e126a241969b3ca8f11499
SiamakFarshidi <informatk.siamak.farshidi@gmail.com>
2022-02-22 08:52:51 +0100

    okay

---
+++ toolSearch/views.py
@@ -0,0 +1,194 @@
+from django.forms.widgets import NullBooleanSelect, Widget
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+import simplejson
+from urllib.request import urlopen
+import urllib
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from glob import glob
+from elasticsearch_dsl import Search, Q, Index
+from elasticsearch_dsl.query import MatchAll
+from django.core import serializers
+import numpy as np
+import json
+import requests
+from bs4 import BeautifulSoup
+from spellchecker import SpellChecker
+# Create your views here.
+es = Elasticsearch("http://localhost:9200")
+#-----------------------------------------------------------------------------------------------------------------------
+
+def genericsearch(request):
+    try:
+        term = request.GET['term']
+    except:
+        term = ''
+    response_data= {}
+
+    if (term=="*"):
+        term=""
+
+    try:
+        term = request.GET['term']
+        term=term.rstrip()
+        term=term.lstrip()
+    except:
+        term = ''
+
+    try:
+        page = request.GET['page']
+    except:
+        page = 0
+
+    try:
+        filter = request.GET['filter']
+    except:
+        filter = ''
+
+    try:
+        facet = request.GET['facet']
+    except:
+        facet = ''
+
+    try:
+        suggestedSearchTerm = request.GET['suggestedSearchTerm']
+    except:
+        suggestedSearchTerm = ''
+
+    searchResults=getSearchResults(request, facet, filter, page, term)
+
+    if(suggestedSearchTerm != ""):
+        searchResults["suggestedSearchTerm"]=""
+    else:
+        suggestedSearchTerm=""
+        if searchResults["NumberOfHits"]==0:
+            suggestedSearchTerm= potentialSearchTerm(term)
+            searchResults=getSearchResults(request, facet, filter, page, "*")
+            searchResults["NumberOfHits"]=0
+            searchResults["searchTerm"]=term
+            searchResults["suggestedSearchTerm"]=suggestedSearchTerm
+
+    return render(request,'tools_results.html',searchResults )
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def getSearchResults(request, facet, filter, page, term):
+    if filter!="" and facet!="":
+        saved_list = request.session['filters']
+        saved_list.append({"term": {facet+".keyword": filter}})
+        request.session['filters'] = saved_list
+    else:
+        if 'filters' in request.session:
+            del request.session['filters']
+        request.session['filters']=[]
+
+    page=(int(page)-1)*10
+    result={}
+    if term=="*" or term=="top10":
+        result = es.search(
+            index="tools",
+            body={
+                "from" : page,
+                "size" : 10,
+                "query": {
+                    "bool" : {
+                        "must" : {
+                            "match_all": {}
+                        }
+                    }
+                }
+            }
+        )
+    else:
+        user_request = "some_param"
+        query_body = {
+            "from" : page,
+            "size" : 10,
+            "query": {
+                "bool": {
+                    "must": {
+                        "multi_match" : {
+                            "query": term,
+                            "fields": [ "name", "description"],
+                            "type": "best_fields",
+                            "minimum_should_match": "50%"
+                        }
+                    },
+                }
+            }
+        }
+
+        result = es.search(index="tools", body=query_body)
+    lstResults=[]
+
+
+    for searchResult in result['hits']['hits']:
+        lstResults.append(searchResult['_source'])
+
+    numHits=result['hits']['total']['value']
+
+    upperBoundPage=round(np.ceil(numHits/10)+1)
+    if(upperBoundPage>10):
+        upperBoundPage=11
+
+    facets=[]
+
+    results={
+        "facets":facets,
+        "results":lstResults,
+        "NumberOfHits": numHits,
+        "page_range": range(1,upperBoundPage),
+        "cur_page": (page/10+1),
+        "searchTerm":term,
+        "functionList": getAllfunctionList(request)
+    }
+
+    return results
+
+#-----------------------------------------------------------------------------------------------------------------------
+def synonyms(term):
+    response = requests.get('https://www.thesaurus.com/browse/{}'.format(term))
+    soup = BeautifulSoup(response.text, 'html.parser')
+    soup.find('section', {'class': 'css-191l5o0-ClassicContentCard e1qo4u830'})
+    return [span.text for span in soup.findAll('a', {'class': 'css-1kg1yv8 eh475bn0'})]
+
+#-----------------------------------------------------------------------------------------------------------------------
+def potentialSearchTerm(term):
+    alternativeSearchTerm=""
+
+    spell = SpellChecker()
+    searchTerm=term.split()
+    alternativeSearchTerm=""
+    for sTerm in searchTerm:
+        alterWord=spell.correction(sTerm)
+        if(alterWord!=""):
+            alternativeSearchTerm= alternativeSearchTerm+" "+alterWord
+
+    alternativeSearchTerm=alternativeSearchTerm.rstrip()
+    alternativeSearchTerm=alternativeSearchTerm.lstrip()
+
+    if alternativeSearchTerm==term:
+        alternativeSearchTerm=""
+        for sTerm in searchTerm:
+            syn=synonyms(sTerm)
+            if len(syn)>0:
+                alterWord=syn[0]
+                alternativeSearchTerm= alternativeSearchTerm+" "+alterWord
+
+    alternativeSearchTerm=alternativeSearchTerm.rstrip()
+    alternativeSearchTerm=alternativeSearchTerm.lstrip()
+
+    return alternativeSearchTerm
+#-----------------------------------------------------------------------------------------------------------------------
+def getAllfunctionList(request):
+    if not 'BasketURLs' in request.session or not request.session['BasketURLs']:
+        request.session['BasketURLs'] = []
+    if not 'MyBasket' in request.session or not request.session['MyBasket']:
+        request.session['MyBasket'] = []
+
+    functionList=""
+    saved_list = request.session['MyBasket']
+    for item in saved_list:
+        functionList= functionList+r"modifyCart({'operation':'add','type':'"+item['type']+"','title':'"+item['title']+"','url':'"+item['url']+"','id':'"+item['id']+"' });"
+    return functionList
